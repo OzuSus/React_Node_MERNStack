0d0f79bc49b70df779e1e9e50bd4b3f5b6667800
OzuSus <21130327@st.hcmuaf.edu.vn>
2026-02-06 19:48:21 +0700

    fix: config DBConection

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./src/config/db";
+import connectDB from "./src/config/db.js";
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 connectDB();
 
 const app = express();
 app.use(express.json());
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Server running on port", PORT);
 });
--- BackEnd/src/config/db.js
+++ BackEnd/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const conectDB = async ()=>{
     try {
-        await mongoose.connect(process.env.DBCONECTIONSTRING);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("DB Conected");
     }catch (err){
         console.error(err)
@@ -10,4 +10,4 @@ const conectDB = async ()=>{
     }
 }
 
-export default conectDB();
+export default conectDB;
