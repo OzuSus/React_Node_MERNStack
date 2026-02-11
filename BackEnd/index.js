b738744f28ab79d1f6309ab985257345259319fd
OzuSus <21130327@st.hcmuaf.edu.vn>
2026-02-11 21:54:55 +0700

    Feat: API register and login acount, API verify acount

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,14 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
+import cors from "cors"
+import authRoutes from "./src/routes/auth.js"
 
 dotenv.config();
-connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
+app.use("/auth", authRoutes);
+
 const PORT = process.env.PORT || 5000;
+connectDB();
 app.listen(PORT, () => {
     console.log("Server running on port", PORT);
 });
