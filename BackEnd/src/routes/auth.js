61348f9f008208f3b0ee181d2037f9b297443a4c
OzuSus <21130327@st.hcmuaf.edu.vn>
2026-02-14 12:57:42 +0700

    Feat: rate Limiting APIs

--- BackEnd/src/routes/auth.js
+++ BackEnd/src/routes/auth.js
@@ -2,12 +2,13 @@ import express from "express";
 import {checkAccount, login, register, verifyEmail} from "../controllers/authController.js";
 import {validate} from "../middlewares/validateMiddleware.js";
 import {loginSchema, registerSchema} from "../validations/authValidation.js";
+import {limiter} from "../middlewares/rateLimiter.js";
 
 const router = express.Router();
 
-router.post("/register", validate(registerSchema), register);
-router.post("/login", validate(loginSchema), login);
-router.get("/verify", verifyEmail);
-router.get("/check-user", checkAccount);
+router.post("/register", limiter, validate(registerSchema), register);
+router.post("/login", limiter, validate(loginSchema), login);
+router.get("/verify", limiter, verifyEmail);
+router.get("/check-user", limiter, checkAccount);
 
 export default router;
