15ab25449b4dc138d04efd78d927ac5303a93a9a
OzuSus <21130327@st.hcmuaf.edu.vn>
2026-03-23 14:49:10 +0700

    manage quantity product in cart

--- BackEnd/src/routes/cart.js
+++ BackEnd/src/routes/cart.js
@@ -1,10 +1,18 @@
 import express from "express";
 import {requireAuth} from "../middlewares/authMiddleware.js";
-import {addToCart, getProductCart} from "../controllers/cartController.js";
+import {
+    addToCart,
+    decreaseProductQuantity,
+    getProductCart,
+    increaseProductQuantity, removeFromCart
+} from "../controllers/cartController.js";
 
 const router = express.Router();
 
 router.get("/", requireAuth, getProductCart);
 router.post("/add", requireAuth, addToCart);
+router.post("/increase", requireAuth, increaseProductQuantity);
+router.post("/decrease", requireAuth, decreaseProductQuantity);
+router.delete("/remove", requireAuth, removeFromCart);
 
 export default router;
--- BackEnd/src/services/cartService.js
+++ BackEnd/src/services/cartService.js
@@ -23,3 +23,46 @@ export async function addToCartService(userId, productId) {
     }
     await CartDetail.create({ id_cart: cart._id, id_product: productId, quantity: 1 });
 }
+
+export async function increaseProductQuantityService(userId, productId) {
+    const cart = await Cart.findOne({id_user: userId});
+    if (!cart) {
+        throw new ApiError(400, "Giỏ hàng không tồn tại");
+    }
+    const cartDetail = await CartDetail.findOne({id_cart: cart._id ,id_product: productId });
+    if (!cartDetail) {
+        throw new ApiError(400, "Sản phẩm không tồn tại trong giỏ hàng");
+    }
+    cartDetail.quantity += 1;
+    await cartDetail.save();
+}
+
+export async function decreaseProductQuantityService(userId, productId) {
+    const cart = await Cart.findOne({id_user: userId});
+    if (!cart) {
+        throw new ApiError(400, "Giỏ hàng không tồn tại");
+    }
+    const cartDetail = await CartDetail.findOne({id_cart: cart._id ,id_product: productId });
+    if (!cartDetail) {
+        throw new ApiError(400, "Sản phẩm không tồn tại trong giỏ hàng");
+    }
+    if(cartDetail.quantity == 1) {
+        await cartDetail.deleteOne();
+        return;
+    }
+    cartDetail.quantity -= 1;
+    await cartDetail.save();
+}
+
+export async function removeFromCartService(userId, productId) {
+    const cart = await Cart.findOne({id_user: userId});
+    if (!cart) {
+        throw new ApiError(400, "Giỏ hàng không tồn tại");
+    }
+    const cartDetail = await CartDetail.findOne({id_cart: cart._id ,id_product: productId });
+    if(!cartDetail) {
+        throw new ApiError(400, "Sản phẩm không tồn tại trong giỏ hàng");
+    }
+    await cartDetail.deleteOne();
+}
+
