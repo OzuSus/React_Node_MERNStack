303196a05e212037f055018a2fd64f5162c38856
OzuSus <21130327@st.hcmuaf.edu.vn>
2026-05-04 10:27:18 +0700

    apply middleware limit for api security

--- BackEnd/src/routes/Order.js
+++ BackEnd/src/routes/Order.js
@@ -3,12 +3,13 @@ import {getAllOrders, getOrderByStatus, getOrderByUser, placeOrder} from "../con
 import {requireAuth} from "../middlewares/authMiddleware.js";
 import {validate} from "../middlewares/validateMiddleware.js";
 import {orderSchema} from "../validations/orderValidation.js";
+import {limiter} from "../middlewares/rateLimiter.js";
 
 const router = express.Router();
 
-router.get("/", getAllOrders);
-router.post("/place", requireAuth, validate(orderSchema), placeOrder)
-router.get("/orderByStatus", requireAuth, getOrderByStatus);
-router.get("/:userId", requireAuth, getOrderByUser);
+router.get("/", limiter, getAllOrders);
+router.post("/place", requireAuth, validate(orderSchema), limiter, placeOrder)
+router.get("/orderByStatus", requireAuth, limiter, getOrderByStatus);
+router.get("/:userId", requireAuth, limiter, getOrderByUser);
 
 export default router;
